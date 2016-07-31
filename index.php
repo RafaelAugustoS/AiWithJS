2cda558c2f4929d82f7671248fa52e104f0c19fd
Rafael Augusto <rafa.2098@gmail.com>
2016-07-30 23:08:25 -0300

    First commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>AI com JS</title>
+	<link rel="stylesheet" href="css/estilo.css">
+	<link href='https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700' rel='stylesheet' type='text/css'>
+</head>
+<body>
+
+	<div class="scene">
+		<div class="grass"></div>
+		<div class="road">
+			<div class="ai">
+				<div class="sensor_1">1</div>
+				<div class="sensor_2">2</div>
+				<div class="sensor_3">3</div>
+			</div>
+			
+			<div class="wall"></div>
+		</div>
+		
+		<div class="grass"></div>
+	</div>
+
+	<script src="ai.js"></script>
+</body>
+</html>
